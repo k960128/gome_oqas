6e9d5c7d7e05a64b61f996a7c544cb1efd3de23c
lixinkang <k_960128@163.com>
2020-09-09 14:26:11 +0800

    李昕慷

--- src/main/java/com/gome/controller/ArgueController.java
+++ src/main/java/com/gome/controller/ArgueController.java
@@ -1,9 +1,7 @@
 package com.gome.controller;
 
-import com.gome.pojo.GomeQaRule;
-import com.gome.pojo.GomeUser;
-import com.gome.pojo.JudgesVote;
-import com.gome.pojo.QaQuestionList;
+import com.gome.pojo.*;
+import com.gome.service.DebateUserSerivce;
 import com.gome.service.GomeUserService;
 import com.gome.service.JudgesVoteService;
 import com.gome.service.QaQuestionListService;
@@ -29,6 +27,8 @@ public class ArgueController {
 
     @Autowired
     private JudgesVoteService judgesVoteService;
+    @Autowired
+    private DebateUserSerivce debateUserSerivce;
 
     @GetMapping("/argue")
     public String toArgue(Model model) {
@@ -42,11 +42,17 @@ public class ArgueController {
         //反方标题
         String title2 = qaQuestionList.getQuestionTitle().split("VS")[1];
         //QaQuestionList qaQuestionList
-        List<GomeUser> gomeUserList = gomeUserService.selectAll();
-        System.out.println(gomeUserList.toString());
-        model.addAttribute("gomeUserList",gomeUserList);
-        model.addAttribute("title1",title1);
-        model.addAttribute("title2",title2);
+        //正方用户数据
+        List<DebateUser> list1 = debateUserSerivce.findAllByStatu(0);
+        System.out.println(list1.toString());
+        //反方用户数据
+        List<DebateUser> list2 = debateUserSerivce.findAllByStatu(1);
+        System.out.println(list2.toString());
+
+        model.addAttribute("list1",list1);
+        model.addAttribute("list2",list2);
+        model.addAttribute("title1", title1);
+        model.addAttribute("title2", title2);
         return "argue";
     }
 
